393593024ecde501b55f0c90d49f727338d7405d
Ravi Tejasvi <ravitejasvi@gmail.com>
2017-12-07 19:11:12 -0500

    Placeholders.

---
+++ supervideo.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import os
+
+def get_videos():
+    directory_path = 'videos'
+    video_ext = ('avi', 'dat', 'mp4', 'mkv', 'vob')
+    videos = []
+    files = os.listdir(directory_path)
+    for f in files:
+        fpath = os.path.join(directory_path, f)
+        if(os.path.isfile(fpath)):
+            if(fpath.endswith(video_ext)):
+                videos.append(f)
+    return videos
+
+if __name__ == '__main__':
+    
+    v = get_videos()
+    if(len(v)==0):
+        print('No videos found in the videos folder. Please add a video and try again.')
+        exit(0)
+    
+    print('Select a video to super resolve. Enter 1 for the first video.')
+    for x in range(1, len(v)+1):
+        print(x, '. ', v[x-1])
+
+    choice = input()
+    print('Selected', choice)
+
+    #Call the matlab package and then call the necessary srgan function.
+
+
+    # Optional open two vlcs to start at the same time in half windows to show the video. One low res and one hight res.
