8fa9906bda1c2c9de5adef11ac48055e538276de
kevinjoseph1995 <kevinjoseph1995@gmail.com>
2018-01-22 23:26:35 -0500

    final commit

--- videos/video_formatting.m
+++ videos/video_formatting.m
@@ -1,13 +1,15 @@
 clc;
 clear;
 close all;
-v_read = VideoReader('football_hq.mp4');
-v_write=VideoWriter('football_lq', 'MPEG-4');
+v_read = VideoReader('bugs_hq.mp4');
+v_write=VideoWriter('bugs_lq', 'MPEG-4');
 v_write.FrameRate=v_read.FrameRate;
 open(v_write)
+i=1;
 while hasFrame(v_read)
     frame = readFrame(v_read);
     resized_frame=imresize(frame,0.25);
-    writeVideo(v_write,resized_frame);      
+    writeVideo(v_write,resized_frame);    
+    i=i+1
 end
 close(v_write)
